a70e4aa66e1365bd25e054631353b4c090e1ceb4
Romain Poultier <poultier.romain@gmail.com>
2020-03-29 03:32:42 +0200

    remove bootstrap implementation datatable

--- admin/bd_gestion.php
+++ admin/bd_gestion.php
@@ -166,7 +166,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.js"></script> 
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+       
         <script type="text/javascript">
             $(document).ready(function () {
                 $('#table_id').DataTable({
